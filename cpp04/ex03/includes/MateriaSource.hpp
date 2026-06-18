c5bd665d8e4376c0cdd5e56918d9a648bf3c8bc4
leogranger <leograngerr@gmail.com>
2026-06-18 14:31:07 +0200

    save

--- cpp04/ex03/includes/MateriaSource.hpp
+++ cpp04/ex03/includes/MateriaSource.hpp
@@ -1,16 +1,17 @@
 #ifndef MATERIASOURCE_HPP
 # define MATERIASOURCE_HPP
 
+# include "IMateriaSource.hpp"
 # include "AMateria.hpp"
 
-class MateriaSource
+class MateriaSource : public IMateriaSource
 {
 	private:
 		AMateria*	inventory[4];
 	public:
 		MateriaSource(void);
 		MateriaSource(const MateriaSource& other);
-		MateriaSource&	operator=(const MateriaSource& otehr);
+		MateriaSource&	operator=(const MateriaSource& other);
 		~MateriaSource(void);
 		void learnMateria(AMateria*);
 		AMateria* createMateria(std::string const & type);
---
+++ cpp04/ex03/srcs/MateriaSource.cpp
@@ -0,0 +1,31 @@
+#include "MateriaSource.hpp"
+
+MateriaSource::MateriaSource(void)
+{
+
+}
+
+MateriaSource::MateriaSource(const MateriaSource& other)
+{
+
+}
+
+MateriaSource&	MateriaSource::operator=(const MateriaSource& other)
+{
+
+}
+
+MateriaSource::~MateriaSource(void)
+{
+
+}
+
+void MateriaSource::learnMateria(AMateria*)
+{
+
+}
+
+AMateria* MateriaSource::createMateria(std::string const & type)
+{
+
+}
