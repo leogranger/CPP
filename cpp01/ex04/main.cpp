d30e0e794d06a04ff76d15f0b9fecf4f3c8f33d4
leogranger <leograngerr@gmail.com>
2026-06-23 12:08:47 +0200

    corrections ex04 cpp01

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -10,6 +10,11 @@ int	main(int ac, char **av)
 	std::string	filename(av[1]);
 	std::string	s1(av[2]);
 	std::string	s2(av[3]);
+	if (s1.empty())
+	{
+		std::cout << "s1 can't be empty." << std::endl;
+		return 0;
+	}
 	Sed	sed(filename, s1, s2);
 	sed.findAndReplace();
 	return (0);
