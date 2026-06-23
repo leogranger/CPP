dfe204bcb883aac000ed44a87e58ba53bc4d55c6
leogranger <leograngerr@gmail.com>
2026-06-23 16:03:00 +0200

    save

--- cpp02/ex00/main.cpp
+++ cpp02/ex00/main.cpp
@@ -6,8 +6,10 @@ int main( void )
 	Fixed b( a );
 	Fixed c;
 	c = b;
+	a.setRawBits(42);
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
+	a = a;
 	return 0;
 }
