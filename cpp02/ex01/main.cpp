5ede51b5ecdf4a45d90809a3782d5c96eba5dfad
leogranger <leograngerr@gmail.com>
2026-06-23 16:09:54 +0200

    corrections

--- cpp02/ex01/main.cpp
+++ cpp02/ex01/main.cpp
@@ -4,18 +4,24 @@ int main( void )
 {
 	Fixed a;
 	Fixed const b( 10 );
-	Fixed const c( 42.42f );
+	Fixed const c( -42.42f );
 	Fixed const d( b );
+	Fixed e(0.5f);
+	Fixed f(0.25f);
 	a = Fixed( 1234.4321f );
 
 	std::cout << "a is " << a << std::endl;
 	std::cout << "b is " << b << std::endl;
 	std::cout << "c is " << c << std::endl;
 	std::cout << "d is " << d << std::endl;
+	std::cout << "e is " << e << std::endl;
+	std::cout << "f is " << f << std::endl;
 
 	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
 	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
 	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
 	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	std::cout << "e is " << e.toFloat() << std::endl;
+	std::cout << "f is " << f.toFloat() << std::endl;
 	return 0;
 }
