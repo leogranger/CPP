808d1586abbb9f5f0ee3646ba34c2932f92ffa55
leogranger <leograngerr@gmail.com>
2026-06-23 11:29:55 +0200

    correctionsssss

--- cpp01/ex01/main.cpp
+++ cpp01/ex01/main.cpp
@@ -2,9 +2,26 @@
 
 int	main(void)
 {
-	int	N = 5;
-	Zombie *horde = zombieHorde(N, "Pablo");
-	for (int i = 0; i < 5; i++)
-    		horde[i].announce();
-	delete[] horde;
+    Zombie *horde;
+
+    std::cout << "=== Horde of 5 ===\n";
+    horde = zombieHorde(5, "Walker");
+
+    for (int i = 0; i < 5; i++)
+        horde[i].announce();
+
+    delete [] horde;
+
+    std::cout << "\n=== Horde of 1 ===\n";
+    horde = zombieHorde(1, "Solo");
+    horde[0].announce();
+    delete [] horde;
+
+    std::cout << "\n=== Horde of 0 ===\n";
+    horde = zombieHorde(0, "Nobody");
+
+    if (!horde)
+        std::cout << "Returned NULL (good)\n";
+
+    return 0;
 }
