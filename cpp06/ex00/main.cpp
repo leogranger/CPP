3afa27bdcfdffb2cf40fae5ab82924f0254f3823
leogranger <leograngerr@gmail.com>
2026-08-18 09:16:35 +0200

    fixed

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -2,11 +2,17 @@
 
 int	main(int ac, char **av)
 {
+	if (ac != 2)
+	{
+		std::cout << "Needs one argument." << std::endl;
+		return (1);
+	}
+	if (!av || !*av)
+	{
+		std::cout << "The argument is empty." << std::endl;
+		return (1);
+	}
 	ScalarConverter Conv;
 	std::string str = av[1];
-	if (ac != 2)
-		return (1);
-	if (!av || !*av)
-		return (1);
 	Conv.convert(str);
 }
